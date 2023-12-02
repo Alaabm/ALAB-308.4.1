9ad2218656130d91e18278cb281915d732dbd99c
Alaabm <alaabasim@gmail.com>
2023-12-01 23:09:56 -0500

    Lab completed

---
+++ practice.js
@@ -0,0 +1,79 @@
+//Part1
+// Split the CSV string into rows based on newline characters.
+const csvString = 
+`ID,Name,Occupation,Age
+42,Bruce,Knight,41
+57,Bob,Fry Cook,19
+63,Blaine,Quiz Master,58
+98,Bill,Doctor’s Assistant,26`;
+let rows = csvString.split("\n");
+for (let i = 0; i < rows.length; i++) {
+  // Split the current row into individual elements (id, name, occupation, age)
+  // based on the comma delimiter and assign them to respective variables.
+  let [cell1, cell2, cell3, cell4] = rows[i].split(",");
+  console.log(cell1, cell2, cell3, cell4);
+}
+// //Part2
+let lines = csvString.split("\n");
+let colNum = lines[0].split(",").length;
+let twoD = [];
+for (let line of lines) {
+  let row = line.split(",");
+  if (row.length === colNum) twoD.push(row);
+}
+console.log(twoD);
+// //Part 3
+//using const map1 = array1.map((x) => x * 2);
+let header = lines[0].split(",");
+let result = [];
+for (let i = 1; i < lines.length; i++) {
+  let values = lines[i].split(",");
+  let dataObj = {};
+  for (let j = 0; j < header.length; j++) {
+    dataObj[header[j]] = values[j];
+  }
+  result.push(dataObj);
+}
+console.log(result);
+// //Part 4
+// let lines = csvString.split(‘\n’);
+// using const map1 = array1.map((x) => x * 2);
+let header1 = lines[0].split(",").map((header1) => header1.toLowerCase());
+console.log("header before the lower-case method:", header1)
+result = [];
+for (let i = 1; i < lines.length; i++) {
+  let values = lines[i].split(",");
+  let dataObj = {};
+  for (let j = 0; j < header1.length; j++) {
+    dataObj[header1[j]] = values[j];
+  }
+  result.push(dataObj);
+
+  //console.log("The result asked by Alaa:", result)
+}
+  console.log("The result asked by Alaa: ",result)
+
+  console.log(result.pop());
+  console.log("After pop:", result);
+  console.log(
+    result.splice(1, 0, {
+      id: "48",
+      name: "Barry",
+      occupation: "Runner",
+      age: "25",
+    })
+  );
+  console.log("After splice:", result);
+  console.log(
+    result.push({ id: "7", name: "Bilbo", occupation: "None", age: "111" })
+  );
+  console.log("After push:", result);
+  let totalAge = 0;
+  for (let i = 0; i < result.length; i++) {
+    totalAge += parseInt(result[i].age);
+    console.log("total ages:", totalAge, result[i].age);
+  }
+  let avgAge = totalAge / result.length;
+  console.log("avgAge:", Math.round(avgAge));
+
+
--- script.js
+++ script.js
@@ -1,48 +1,74 @@
-//Part 2
-const string = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`;
-let splitString = string.split("\n");
+///////////Part1
+const string = 
+`ID,Name,Occupation,Age
+42,Bruce,Knight,41
+57,Bob,Fry Cook,19
+63,Blaine,Quiz Master,58
+98,Bill,Doctor’s Assistant,26`;
+let rows = string.split("\n");
+for (let i = 0; i < rows.length; i++) {
+  // Split  into individual elements (id, name, occupation, age)
+  // Assign them to respective variables.
+let [cell1, cell2, cell3, cell4] = rows[i].split(",");
+console.log(cell1, cell2, cell3, cell4);
+}
+//////////Part2
+let lines = string.split("\n");
+let colNum = lines[0].split(",").length;
 let twodArray = [];
-console.log(splitString);
-splitString.forEach((stringSection) => {
-twodArray.push(stringSection.split(','));
-});
+for (let line of lines) {
+let row = line.split(",");
+if (row.length === colNum) twodArray.push(row);
+}
 console.log(twodArray);
+/////////Part 3
+let header = lines[0].split(",");
+let result = [];
+for (let i = 1; i < lines.length; i++) {
+let values = lines[i].split(",");
+let dataObj = {};
+for (let j = 0; j < header.length; j++) {
+    dataObj[header[j]] = values[j];
+}
+result.push(dataObj);
+}
+console.log(result);
+/////////Part 4
+// let lines = csvString.split(‘\n’);
+// using const map1 = array1.map((x) => x * 2);
+let header1 = lines[0].split(",").map((header1) => header1.toLowerCase());
+console.log("header|lower-case method:", header1)
+result = [];
+for (let i = 1; i < lines.length; i++) {
+let values = lines[i].split(",");
+let dataObj = {};
+for (let j = 0; j < header1.length; j++) {
+    dataObj[header1[j]] = values[j];
+}
+result.push(dataObj);
 
+}
+console.log("Organized Array with variables: ",result)
 
-
-// let cell1 = "";
-// let cell2 = "";
-// let cell3 = "";
-// let cell4 = "";
-
-
-// for(let i = 0; i <string.length; i++ ){
-//     while(string[i] === '\n' ){
-//     console.log(cell1, cell2, cell3, cell4);
-// }
-// }
-
-
-// let counter = 1
-// for(let i = 0; i <string.length; i++ ){
-//     if(counter === 1){
-//         cell1 = cell1 + string[i];
-//         //add in cell 1
-//     }
-//     if(counter === 2 ){
-//         cell2 = cell2 + string[i];
-//         //add in cell 2
-//     }
-//     if(counter === 3){
-//         cell3 = cell3 + string[i];
-//         //add in cell 3
-//     }
-//     if(counter === 4){
-//         cell4 = cell4 + string[i];
-//         //add in cell 4
-//     }
-//     if(string[i] === '\n'){
-//         counter += 1;
-//     }
-// }
-// console.log(cell1, cell2, cell3, cell4);
+console.log(result.pop());
+console.log("After pop:", result);
+console.log(
+    result.splice(1, 0, {
+    id: "48",
+    name: "Barry",
+    occupation: "Runner",
+    age: "25",
+    })
+);
+console.log("After splice:", result);
+console.log(
+    result.push({ id: "7", name: "Bilbo", occupation: "None", age: "111" })
+);
+console.log("After push:", result);
+let totalAge = 0;
+for (let i = 0; i < result.length; i++) {
+    totalAge += parseInt(result[i].age);
+    console.log("total ages:", totalAge, result[i].age);
+}
+let avgAge = totalAge / result.length;
+console.log("avgAge:", Math.round(avgAge));
